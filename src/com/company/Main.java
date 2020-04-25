68930ba7da36a26924ab3960733520f9ac22e83c
Andrey Mukhin <mukhinaa89@yandex.ru>
2020-04-25 16:46:12 +0300

    unknown commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,36 @@
 package com.company;
-import java.math.*;
+import java.lang.Integer;
 public class Main {
 
     public static void main(String[] args) {
-        double a = 0.1;
+        /*double a = 0.1;
         double b = 0.2;
         double c = 0.3;
         boolean d = Math.abs(a+b-c) < 1E-4;
-        System.out.println(d);
-        };
+        System.out.println(d);*/
+
+        //double e = 0x0bp3;
+       // System.out.println(0x0bp3);
+        {
+            int a = 100;
+
+            // Преобразование целого числа в двоичный формат
+            System.out.println(Integer.toBinaryString(a));
+
+            // напечатать число 1 в числе a
+            System.out.println(Integer.bitCount(a));
+        }
+
     }
+
+    // Java-программа для демонстрации работы
+// метода java.lang.Integer.bitCount ()
+
+
+   /* class Gfg {
+        // код драйвера
+        public static void main(String args[])
+
+
+    } */
+}
